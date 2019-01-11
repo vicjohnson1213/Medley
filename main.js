907e564cb4597a771d0f13d14976d00fa32f4c81
Victor Johnson <vicjohnson1213@gmail.com>
2019-01-11 16:25:18 -0600

    Working electron app.

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+let mainWindow;
+
+function createMainWindow() {
+    mainWindow = new BrowserWindow({ width: 800, height: 600 });
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, '/dist/index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createMainWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createMainWindow();
+    }
+});
