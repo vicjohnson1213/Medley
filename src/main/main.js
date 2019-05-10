1ace03a0f0f0ae7d27283ca8add829d88f6d77d0
Victor Johnson <vicjohnson1213@gmail.com>
2019-05-10 02:08:07 -0500

    More menu items.

--- src/main/main.js
+++ src/main/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Menu, ipcMain } = require('electron');
+const { app, BrowserWindow, Menu, ipcMain, shell } = require('electron');
 const path = require('path');
 const url = require('url');
 const fs = require('fs').promises;
@@ -7,11 +7,11 @@ let mainWindow;
 
 app.on('ready', createMainWindow);
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
-});
+// app.on('window-all-closed', () => {
+//     if (process.platform !== 'darwin') {
+//         app.quit();
+//     }
+// });
 
 app.on('activate', () => {
     if (mainWindow === null) {
@@ -20,7 +20,11 @@ app.on('activate', () => {
 });
 
 function createMainWindow() {
-    mainWindow = new BrowserWindow({ width: 800, height: 600 });
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        title: 'Markdown Editor'
+    });
 
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, '../../dist/index.html'),
@@ -32,8 +36,7 @@ function createMainWindow() {
         mainWindow = null;
     });
 
-    Menu.setApplicationMenu(createMenu());
-    mainWindow.webContents.openDevTools();
+    createMenu();
 
     ipcMain.on('getNotes', (event, args) => {
         getNotes();
@@ -91,26 +94,127 @@ function buildTreeP(dir) {
 }
 
 function createMenu() {
-    return Menu.buildFromTemplate([{
+    const template = [{
         label: 'File',
         submenu: [
             { label: 'New Note' }
         ]
     }, {
-        label: 'Reload',
-        click: () => {
-            console.log('reloading');
-            mainWindow.loadURL(url.format({
-                pathname: path.join(__dirname, '../../dist/index.html'),
-                protocol: 'file',
-                slashes: true
-            }));
-        }
+        label: 'Edit',
+        submenu: [
+            { role: 'undo' },
+            { role: 'redo' },
+            { type: 'separator' },
+            { role: 'cut' },
+            { role: 'copy' },
+            { role: 'paste' },
+            { role: 'delete' },
+            { role: 'selectall' }
+        ]
     }, {
-        label: 'Fetch',
-        click: () => {
-            console.log('fetching');
-            getNotes();
+        label: 'View',
+        submenu: [
+            {
+                label: 'Reload',
+                accelerator: 'CmdOrCtrl+R',
+                click:  (item, focusedWindow) => {
+                    mainWindow.loadURL(url.format({
+                        pathname: path.join(__dirname, '../../dist/index.html'),
+                        protocol: 'file',
+                        slashes: true
+                    }));
+                }
+            }, {
+                type: 'separator'
+            }, { 
+                role: 'resetzoom'
+            }, { 
+                role: 'zoomin'
+            }, { 
+                role: 'zoomout'
+            }, { 
+                type: 'separator'
+            }, { 
+                role: 'togglefullscreen'
+            }
+        ]
+    }, {
+        role: 'window',
+        submenu: [
+            {
+                role: 'minimize'
+            }, {
+                role: 'close'
+            }
+        ]
+    }, {
+        label: 'Dev',
+        submenu: [{
+            label: 'Reload UI',
+            click: () => {
+                console.log('reloading');
+                mainWindow.loadURL(url.format({
+                    pathname: path.join(__dirname, '../../dist/index.html'),
+                    protocol: 'file',
+                    slashes: true
+                }));
+            }
+        }, {
+            label: 'Toggle Developer Tools',
+            accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
+            click: (item, focusedWindow) => {
+                if (focusedWindow) {
+                    focusedWindow.webContents.toggleDevTools();
+                }
+            }
+        }]
+    }, {
+      role: 'help',
+      submenu: [
+        {
+            label: 'Learn More',
+            click: () => { shell.openExternal('http://electron.atom.io') }
         }
-    }]);
+      ]
+    }];
+
+    if (process.platform === 'darwin') {
+        const name = app.getName();
+        template.unshift({
+            label: name,
+            submenu: [
+                { role: 'about' },
+                { type: 'separator' },
+                { role: 'services', submenu: [] },
+                { type: 'separator' },
+                { role: 'hide' },
+                { role: 'hideothers' },
+                { role: 'unhide' },
+                { type: 'separator' },
+                { role: 'quit' },
+            ]
+        });
+
+        template[4].submenu = [
+            {
+              label: 'Close',
+              accelerator: 'CmdOrCtrl+W',
+              role: 'close'
+            }, {
+              label: 'Minimize',
+              accelerator: 'CmdOrCtrl+M',
+              role: 'minimize'
+            }, {
+              label: 'Zoom',
+              role: 'zoom'
+            }, {
+              type: 'separator'
+            }, {
+              label: 'Bring All to Front',
+              role: 'front'
+            }
+        ];
+    }
+
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 }
